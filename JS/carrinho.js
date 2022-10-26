a6c2868b44695eced40eba5b3153fde248000f66
Diego Alcântara <ddag0309@gmail.com>
2022-10-26 13:19:36 -0300

    Estilo e funcionalidade do carrinho.
    Aprimorado design do carrinho.
    
    Carrinho aparece na tela apenas quando o usuário o chama pela navbar.

--- JS/carrinho.js
+++ JS/carrinho.js
@@ -1,3 +1,15 @@
+// Botão carrinho na navbar.
+let abrirCarrinhoBtn = document.getElementById("abrirCarrinhoBtn").addEventListener("click", mostrarEsconderCarrinho);
+// Botão "x" no carrinho.
+let fecharCarrinhoBtn = document.getElementById("fecharCarrinhoBtn").addEventListener("click", mostrarEsconderCarrinho);
+
+// Abre o carrinho caso esteja fechado, e fecha caso esteja aberto.
+function mostrarEsconderCarrinho(){
+    let carrinho = document.getElementById("botao__carinho")
+    carrinho.classList.toggle("carrinhoAberto");
+}
+
+
 function AddCarrinho(produto, qtd, valor, posicao) {
     localStorage.setItem("produto" + posicao, produto);
     localStorage.setItem("qtd" + posicao, qtd);
