1957eb69509c3349723eba4e2dc1e60ba1122158
Diogo Melo <98532975+Digao46@users.noreply.github.com>
2022-11-01 14:10:30 -0300

    Correção no css de duas páginas

--- JS/carrinho.js
+++ JS/carrinho.js
@@ -1,19 +1,22 @@
 // Botão carrinho na navbar.
-let abrirCarrinhoBtn = document.getElementById("abrirCarrinhoBtn").addEventListener("click", mostrarEsconderCarrinho);
+let abrirCarrinhoBtn = document
+  .getElementById("abrirCarrinhoBtn")
+  .addEventListener("click", mostrarEsconderCarrinho);
 // Botão "x" no carrinho.
-let fecharCarrinhoBtn = document.getElementById("fecharCarrinhoBtn").addEventListener("click", mostrarEsconderCarrinho);
+let fecharCarrinhoBtn = document
+  .getElementById("fecharCarrinhoBtn")
+  .addEventListener("click", mostrarEsconderCarrinho);
 
 // Abre o carrinho caso esteja fechado, e fecha caso esteja aberto.
-function mostrarEsconderCarrinho(){
-    let carrinho = document.getElementById("botao__carinho")
-    carrinho.classList.toggle("carrinhoAberto");
+function mostrarEsconderCarrinho() {
+  let carrinho = document.getElementById("botao__carinho");
+  carrinho.classList.toggle("carrinhoAberto");
 }
 
-
 function AddCarrinho(produto, qtd, valor, posicao) {
-    localStorage.setItem("produto" + posicao, produto);
-    localStorage.setItem("qtd" + posicao, qtd);
-    valor = valor * qtd;
-    localStorage.setItem("valor" + posicao, valor);
-    alert("Produto adicionado ao carrinho!");
+  localStorage.setItem("produto" + posicao, produto);
+  localStorage.setItem("qtd" + posicao, qtd);
+  valor = valor * qtd;
+  localStorage.setItem("valor" + posicao, valor);
+  alert("Produto adicionado ao carrinho!");
 }
--- JS/navbar.js
+++ JS/navbar.js
@@ -1,16 +1,19 @@
-document.getElementById("produtosListaDropDown").addEventListener("mouseenter", ()=>{
-
+document
+  .getElementById("produtosListaDropDown")
+  .addEventListener("mouseenter", () => {
     document.getElementById("produtosListaDropDownUl").classList.add("mostrar");
+  });
+
+document
+  .getElementById("produtosListaDropDown")
+  .addEventListener("mouseleave", () => {
+    document
+      .getElementById("produtosListaDropDownUl")
+      .classList.remove("mostrar");
+  });
+
+document.getElementById("hamburguerBtn").addEventListener("click", () => {
+  document.getElementById("hamburguerBtn").classList.toggle("ativo");
+
+  document.getElementsByClassName("navMenu")[0].classList.toggle("mostrar");
 });
-
-document.getElementById("produtosListaDropDown").addEventListener("mouseleave", ()=>{
-
-    document.getElementById("produtosListaDropDownUl").classList.remove("mostrar");
-})
-
-document.getElementById("hamburguerBtn").addEventListener("click", () =>{
-
-    document.getElementById("hamburguerBtn").classList.toggle("ativo");
-
-    document.getElementsByClassName("navMenu")[0].classList.toggle("mostrar");
-})
