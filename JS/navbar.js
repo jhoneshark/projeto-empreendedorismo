d5d4985c672ae8499d59cc0001c024668d6c5b67
Diego Alcântara <ddag0309@gmail.com>
2022-11-08 09:41:45 -0300

    Itens no carrinho.
    Adicionado modelo de itens para adições no carrinho.

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,19 +1,25 @@
-document
-  .getElementById("produtosListaDropDown")
-  .addEventListener("mouseenter", () => {
-    document.getElementById("produtosListaDropDownUl").classList.add("mostrar");
-  });
-
-document
-  .getElementById("produtosListaDropDown")
-  .addEventListener("mouseleave", () => {
-    document
-      .getElementById("produtosListaDropDownUl")
-      .classList.remove("mostrar");
-  });
-
 document.getElementById("hamburguerBtn").addEventListener("click", () => {
   document.getElementById("hamburguerBtn").classList.toggle("ativo");
 
   document.getElementsByClassName("navMenu")[0].classList.toggle("mostrar");
 });
+
+  try {
+    
+    document
+    .getElementById("produtosListaDropDown")
+    .addEventListener("mouseenter", () => {
+      
+    document.getElementById("produtosListaDropDownUl").classList.add("mostrar");
+    });
+
+  document
+  .getElementById("produtosListaDropDown")
+  .addEventListener("mouseleave", () => {
+
+    document.getElementById("produtosListaDropDownUl").classList.remove("mostrar");
+  });
+
+  } catch (error) {
+    console.log(error);
+  }
