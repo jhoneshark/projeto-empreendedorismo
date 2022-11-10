84f1cbe3724ca758109cbfc3b642a9f049f83f76
Diego Alcântara <ddag0309@gmail.com>
2022-11-10 09:59:47 -0300

    Bug corrigido.
    removido bug da navbar.

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,8 +1,4 @@
-document.getElementById("hamburguerBtn").addEventListener("click", () => {
-  document.getElementById("hamburguerBtn").classList.toggle("ativo");
 
-  document.getElementsByClassName("navMenu")[0].classList.toggle("mostrar");
-});
 
   try {
     
