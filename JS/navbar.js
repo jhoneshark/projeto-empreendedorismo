f57b1ee121a75675bd44bcad342e6e6dcf2102ac
Diego Alcântara <ddag0309@gmail.com>
2022-11-11 10:39:16 -0300

    Adicionado autoscroll na navbar.

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,21 +1,56 @@
 
-
-  try {
+// try catch é utilizado para não ter erro com os outros arquivos html, pois as funções presentes nele apenas funcionam no index.html
+try {
+  
+  // Mostra ou esconde o dropdown dos destaques da navbar na home section
+  document
+  .getElementById("produtosListaDropDown")
+  .addEventListener("mouseenter", () => {
     
-    document
-    .getElementById("produtosListaDropDown")
-    .addEventListener("mouseenter", () => {
-      
-    document.getElementById("produtosListaDropDownUl").classList.add("mostrar");
-    });
-
-    document
-    .getElementById("produtosListaDropDown")
-    .addEventListener("mouseleave", () => {
-
-    document.getElementById("produtosListaDropDownUl").classList.remove("mostrar");
+  document.getElementById("produtosListaDropDownUl").classList.add("mostrar");
   });
 
-  } catch (error) {
-    console.log(error);
+  document
+  .getElementById("produtosListaDropDown")
+  .addEventListener("mouseleave", () => {
+
+  document.getElementById("produtosListaDropDownUl").classList.remove("mostrar");
+});
+
+
+
+// AutoScroll para a section desejada de acordo com o botão clicado nos destaques da navbar da home section
+let promocaoBtn = document.getElementById("promocaoBtn");
+promocaoBtn.addEventListener("click", () =>{autoscroll("promocao")})
+
+let maisVendidosBtn = document.getElementById("maisVendidosBtn");
+maisVendidosBtn.addEventListener("click", () =>{autoscroll("maisVendidos")});
+
+
+} catch (error) {
+  console.log(error);
+  console.log("Dropdown da navbar não localizado");
+}
+
+
+function autoscroll(section){
+
+  switch (section) {
+    case "promocao":
+
+      let promocaoSection = document.getElementsByClassName("produtosDestaqueSection")[0];
+
+      promocaoSection.scrollIntoView(false);
+      break;
+
+    case "maisVendidos":
+
+      let maisVendidosSection = document.getElementsByClassName("maisVendidosSection")[0];
+
+      maisVendidosSection.scrollIntoView(true);
+      break
+  
+    default:
+      break;
   }
+}
